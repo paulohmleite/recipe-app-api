2153ac3b5d0b27f428ff0613f98a175353561f8a
phmleite <paulohmleite1@gmail.com>
2023-01-14 13:19:14 -0400

    Some changes on docker image.

---
+++ app/app/calc.py
@@ -0,0 +1,23 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """
+    Add x and y return result.
+    :param x:
+    :param y:
+    :return x + y:
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """
+    Subtract x from y and return result
+    :param x:
+    :param y:
+    :return result:
+    """
+    return y - x
