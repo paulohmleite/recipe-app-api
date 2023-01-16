7f802a7fc4fbffb5cdca7b5ba4825fd85f4512b3
phmleite <paulohmleite1@gmail.com>
2023-01-16 14:09:13 -0400

    inclusao de testes de usuario api

--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,7 @@
 Views for the user API.
 """
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -20,3 +20,16 @@ class CreateTokenView(ObtainAuthToken):
 
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+
+class ManagerUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    def get_object(self):
+        """Revrieve and return the authenticated user."""
+        return self.request.user
