57981c5a4da9a3b676c25dea4b05ec82cc1befe0
phmleite <paulohmleite1@gmail.com>
2023-01-16 10:04:00 -0400

    created tests for user

--- app/core/models.py
+++ app/core/models.py
@@ -10,12 +10,15 @@ from django.contrib.auth.models import (
 )
 
 
+# GOOD PRACTICES
 class UserManager(BaseUserManager):
     """Manager for users."""
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
